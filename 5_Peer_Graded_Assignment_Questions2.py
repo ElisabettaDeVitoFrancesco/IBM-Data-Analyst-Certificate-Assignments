4c3fcd5faddbe42708560d9278a2a7dcadf82c6b
ElisabettaDeVitoFrancesco <elisabetta.devitof@gmail.com>
2022-12-05 09:57:18 +0100

    Update 5_Peer_Graded_Assignment_Questions2.py

--- 5_Peer_Graded_Assignment_Questions2.py
+++ 5_Peer_Graded_Assignment_Questions2.py
@@ -166,7 +166,7 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
             
             # TASK5: Average flight time by reporting airline
             # Enter your code below. Make sure you have correct formatting.
-            line_fig = px.line(line_data, x='Month', y='AirTime', color='Reporting_Airline ', title='Average monthly flight time (minutes) by airline')
+            line_fig = px.line(line_data, x='Month', y='AirTime', color='Reporting_Airline', title='Average monthly flight time (minutes) by airline')
             
             # Percentage of diverted airport landings per reporting airline
             pie_fig = px.pie(div_data, values='Flights', names='Reporting_Airline', title='Percentage of flights by reporting airline')
@@ -187,7 +187,7 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
             # Enter your code below. Make sure you have correct formatting.
             tree_fig = px.treemap(tree_data, path=['DestState', 'Reporting_Airline'], 
                       values='Flights',
-                      color='Flights ',
+                      color='Flights',
                       color_continuous_scale='RdBu',
                       title='Flight count by airline to destination state'
                 )
